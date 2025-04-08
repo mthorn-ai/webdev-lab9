6762a097e7adcdb104a47d4615ea8e2d03d581d9
Matthew Thornton <thorntonm244@gmail.com>
2025-04-08 14:04:59 -0500

    completed lab

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 function init(){
-//add your javascrip between these two lines of code
- 
+ var button = document.getElementById('entrybutton');
 
-
+  function showText(){
+    var textbox = document.getElementById('entryinput');
+    document.getElementById('textoutput').innerHTML = textbox.value;
+    alert("Matthew Thornton: " + textbox.value);
+  }
+  
+  button.addEventListener('click', showText);
+}
 
 
 
